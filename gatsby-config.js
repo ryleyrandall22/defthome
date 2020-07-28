6a305d656fa437b5dc4afeff79fb258d8f36962f
ryleyrandall22 <33163774+ryleyrandall22@users.noreply.github.com>
2020-07-28 15:44:43 -0700

    Landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,27 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Node.js (TypeScript) API',
+    title: "Gatsby + Node.js (TypeScript) API",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Gatsby + Node.js (TypeScript) API',
-        short_name: 'Gatsby + Node.js (TypeScript)',
-        start_url: '/',
-        icon: 'src/images/gatsby-icon.png',
+        name: "Gatsby + Node.js (TypeScript) API",
+        short_name: "Gatsby + Node.js (TypeScript)",
+        start_url: "/",
+        icon: "src/images/gatsby-icon.png",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`400`, "500", `700`, "800"],
+          },
+        ],
       },
     },
   ],
